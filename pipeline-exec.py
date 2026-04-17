b88348df4a3c255d1b89c10a945c8f825ec4c98a
Om Prakash Chand <omprakashchand@Oms-MacBook-Pro.local>
2026-04-17 23:09:57 +0530

    file imported in pipeline-1

--- pipeline-exec.py
+++ pipeline-exec.py
@@ -1,12 +1,12 @@
 import fetchdata
 
 def run_ingestion():
-    files = list_cur_files()
+    files = fetchdata.list_cur_files()
     
     for file in files:
-        df = load_parquet_from_s3(file)
-        df = filter_columns(df)
-        save_to_db(df)
+        df = fetchdata.load_parquet_from_s3(file)
+        df = fetchdata.filter_columns(df)
+        fetchdata.save_to_db(df)
 
 if __name__ == "__main__":
     run_ingestion()
